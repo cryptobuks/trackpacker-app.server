205cf760a6d4be95c2697c169496cf10b4865299
Invincible_Pain <terrorgeek@gmail.com>
2015-05-01 23:46:04 -0400

    a
    a

---
+++ couchdb_conf.js
@@ -0,0 +1,17 @@
+var nano = require('nano');
+var username="whinswallsistanceptelmos";
+var password="irKXPnuCH8XPTQiUHfbtbvlV";
+var db_url="@trackpacker.cloudant.com";
+
+var CouchDB=function(){
+    return {db_conn_str: "http://" + username + ":" + password + db_url,
+        db: function (database_name) {
+            return nano(this.db_conn_str + "/" + database_name)
+        }
+    }
+};
+
+//console.log(CouchDB.db("users"));
+//console.log(CouchDB.db_conn_str);
+
+module.exports=CouchDB;
