baa44a8312b25ee42d0f7cbbac5aa5b087c8000d
Yu Song <yu.song@theeagleforce.net>
2015-05-02 21:26:34 -0400

    a

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var express=require("express");
 var CouchDB=require("./couchdb_conf");
+var bodyParser = require('body-parser')
+
+//require the custom modules
+var login=require("./authenticate/login");
 
 var app=express();
-app.get("/login", function(request, response){
-    var nano=CouchDB().db("users");
-//    nano.insert({nimabi: "daye"}, function(err, body, header){
-//        console.log(body);
-//        response.json(body);
-//    })
-    nano.get({name: "songyu"}, function(err, body){
-        response.json(body);
-    });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+//use all the custom modules
+login(app, CouchDB);
 
 app.listen(3000);
---
+++ authenticate/login.js
@@ -0,0 +1,26 @@
+var login=function(app, CouchDB){
+  var nano=CouchDB().db("users");
+  app.get("/users/login/:email/:password", function(request, response){
+
+    nano.view('login', 'by_email_and_password', {key:[request.params.email, request.params.password]}, function(err, body) {
+      if (err) console.log(err);
+         if (!body.rows.length) {
+            response.json(body);
+         } else {
+            response.status(200).json(body);
+         }
+      });
+  });
+
+  app.post("/users/signup", function(request, response){
+      var req_body=request.body;
+      nano.insert({first_name: req_body.first_name, last_name: req_body.last_name,
+                   email: req_body.email, password: req_body.password,
+                   sync_url: "https://trackpacker.cloudant.com/users"},
+                   function(request, response){
+
+      });
+  });
+}
+
+module.exports=login;
