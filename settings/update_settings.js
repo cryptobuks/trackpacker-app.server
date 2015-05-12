773bf6ad919074f2b64e45ebe63eb713dfc82506
Invincible_Pain <terrorgeek@gmail.com>
2015-05-12 10:36:29 -0400

    a

--- settings/update_settings.js
+++ settings/update_settings.js
@@ -32,20 +32,21 @@ var settings=function(app, CouchDB){
     var nano=CouchDB().db("settings");
     app.post("/settings/update", function(request, response){
         var req_body=request.body;
-        console.log(nano);
-        nano.insert({
-          '_rev': '1-6e592366b4586cf82308d57814039b65',
-          active_map: req_body.trip_id, //a foreign key to the active trip _id
-          gps_intervals: { //used as defaults after first trip is started,         
-            distance:2000, //meters of distance between auto lookup
-            frequency: req_body.save_frequency //the number of minutes between auto lookup
-          },
-          auto_gps_tracking: req_body.auto_gps_tracking,
-          safty_checkin: req_body.safty_checkin
-        }, function(request, response){  });
+        nano.get(req_body.user_id, function(err, doc){
+           updaterev = doc._rev;
+           nano.insert({
+              _rev: updaterev,
+              active_map: req_body.trip_id, //a foreign key to the active trip _id
+              gps_intervals: { //used as defaults after first trip is started,         
+                 distance:2000, //meters of distance between auto lookup
+                 frequency: req_body.save_frequency //the number of minutes between auto lookup
+              },
+              auto_gps_tracking: req_body.auto_gps_tracking,
+              safty_checkin: req_body.safty_checkin
+           }, req_body.user_id ,function(err, body, header){  });
+        });
     });
 }
-
 
 // users.get('document_name', function(err, doc) {
 //         updaterev = doc._rev;
