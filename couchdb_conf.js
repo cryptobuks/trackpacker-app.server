94d342f82511fe596ff1db7fae9f936a063e8966
yusong1 <bjslxxx@126.com>
2015-07-02 00:15:56 -0400

    a

--- couchdb_conf.js
+++ couchdb_conf.js
@@ -1,10 +1,21 @@
+//===============remote cloudant============
 var nano = require('nano');
 var username="whinswallsistanceptelmos";
 var password="irKXPnuCH8XPTQiUHfbtbvlV";
 var db_url="@trackpacker.cloudant.com";
 
+//var CouchDB = function(){
+//    return {db_conn_str: "http://" + username + ":" + password + db_url,
+//        db: function (database_name) {
+//            return nano(this.db_conn_str + "/" + database_name)
+//        }
+//    }
+//};
+
+//===============localhost=================
+var localhost="http://localhost:5984";
 var CouchDB = function(){
-    return {db_conn_str: "http://" + username + ":" + password + db_url,
+    return {db_conn_str: localhost,
         db: function (database_name) {
             return nano(this.db_conn_str + "/" + database_name)
         }
--- cradle.js
+++ cradle.js
@@ -1,3 +1,4 @@
+//================remote cloudant=================
 var cradle_db = require("cradle");
 
 var username="whinswallsistanceptelmos";
@@ -5,9 +6,19 @@ var password="irKXPnuCH8XPTQiUHfbtbvlV";
 var db_url="@trackpacker.cloudant.com";
 
 
+//var cradle = function() {
+//    return new(cradle_db.Connection)('https://trackpacker.cloudant.com', 443, {
+//        auth: { username: username, password: password },
+//        cache: true,
+//        raw: false,
+//        forceSave: true
+//    });
+//};
+
+//==================localhost====================
+var localhost="http://localhost";
 var cradle = function() {
-    return new(cradle_db.Connection)('https://trackpacker.cloudant.com', 443, {
-        auth: { username: username, password: password },
+    return new(cradle_db.Connection)(localhost, 5984, {
         cache: true,
         raw: false,
         forceSave: true
